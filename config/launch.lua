b9969b055c9444c7e19a88f5738a58a5a271a56d
Micah Bucy <micah.bucy@theeternalsw0rd.rocks>
2025-10-30 15:02:56 -0400

    add powershell to launch menu on non-windows machines

--- config/launch.lua
+++ config/launch.lua
@@ -1,36 +1,38 @@
 local platform = require('utils.platform')
 
 local options = {
-   default_prog = {},
-   launch_menu = {}
+  default_prog = {},
+  launch_menu = {}
 }
 
 if platform.is_win then
-   options.default_prog = { 'pwsh', '-NoLogo' }
-   options.launch_menu = {
-      { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
-      { label = 'PowerShell Desktop', args = { 'powershell' } },
-      { label = 'Command Prompt', args = { 'cmd' } },
-      { label = 'Nushell', args = { 'nu' } },
-      { label = 'UCRT64 / MSYS2', args = { 'msys2_shell.cmd', '-use-full-path', '-defterm', '-here', '-no-start', '-ucrt64', '-shell', 'fish' } },
-      { label = 'MSYS / MSYS2', args = { 'msys2_shell.cmd', '-use-full-path', '-defterm', '-here', '-no-start', '-msys', '-shell', 'fish' } },
-   }
+  options.default_prog = { 'pwsh', '-NoLogo' }
+  options.launch_menu = {
+    { label = 'PowerShell Core',    args = { 'pwsh', '-NoLogo' } },
+    { label = 'PowerShell Desktop', args = { 'powershell' } },
+    { label = 'Command Prompt',     args = { 'cmd' } },
+    { label = 'Nushell',            args = { 'nu' } },
+    { label = 'UCRT64 / MSYS2',     args = { 'msys2_shell.cmd', '-use-full-path', '-defterm', '-here', '-no-start', '-ucrt64', '-shell', 'fish' } },
+    { label = 'MSYS / MSYS2',       args = { 'msys2_shell.cmd', '-use-full-path', '-defterm', '-here', '-no-start', '-msys', '-shell', 'fish' } },
+  }
 elseif platform.is_mac then
-   options.default_prog = { 'fish', '-l' }
-   options.launch_menu = {
-      { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { 'fish', '-l' } },
-      { label = 'Nushell', args = { '/opt/homebrew/bin/nu', '-l' } },
-      { label = 'Zsh', args = { 'zsh', '-l' } },
-   }
+  options.default_prog = { 'fish', '-l' }
+  options.launch_menu = {
+    { label = 'Bash',            args = { 'bash', '-l' } },
+    { label = 'Fish',            args = { 'fish', '-l' } },
+    { label = 'Nushell',         args = { '/opt/homebrew/bin/nu', '-l' } },
+    { label = 'Zsh',             args = { 'zsh', '-l' } },
+    { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
+  }
 elseif platform.is_linux then
-   options.default_prog = { 'fish', '-l' }
-   options.launch_menu = {
-      { label = 'Bash', args = { 'bash', '-l' } },
-      { label = 'Fish', args = { 'fish', '-l' } },
-      { label = 'Nushell', args = { 'nu', '-l' } },
-      { label = 'Zsh', args = { 'zsh', '-l' } },
-   }
+  options.default_prog = { 'fish', '-l' }
+  options.launch_menu = {
+    { label = 'Bash',            args = { 'bash', '-l' } },
+    { label = 'Fish',            args = { 'fish', '-l' } },
+    { label = 'Nushell',         args = { 'nu', '-l' } },
+    { label = 'Zsh',             args = { 'zsh', '-l' } },
+    { label = 'PowerShell Core', args = { 'pwsh', '-NoLogo' } },
+  }
 end
 
 return options
