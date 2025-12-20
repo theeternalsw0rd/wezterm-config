5802e86179c76f975c58c21e8189492c0ce7c45f
Micah Bucy <micah.bucy@theeternalsw0rd.rocks>
2025-12-20 03:45:45 -0500

    switch maximize method so it works on Windows without making WezTerm completely transparent

--- config/init.lua
+++ config/init.lua
@@ -1,9 +1,16 @@
 local wezterm = require('wezterm')
+local platform = require('utils.platform')
+
 local mux = wezterm.mux
 
-wezterm.on("gui-startup", function(cmd)
-    local tab, pane, window = mux.spawn_window(cmd or {})
-    window:gui_window():maximize()
+wezterm.on('gui-attached', function(domain)
+  -- maximize all displayed windows on startup
+  local workspace = mux.get_active_workspace()
+  for _, window in ipairs(mux.all_windows()) do
+    if window:get_workspace() == workspace then
+      window:gui_window():maximize()
+    end
+  end
 end)
 
 ---@class Config
